8106350d706b277eac8b64f2beaec20a290ce282
Xennis <xennis@online.de>
2013-09-29 00:10:23 +0200

    .

--- js/timer.js
+++ js/timer.js
@@ -1,9 +1,8 @@
 GAME.timer = function(jQuery, shapeNumber){
+
 	var countdown;
 	var incrementTime = window.GAME.settings.incrementTime;
 	var currentTime = shapeNumber*window.GAME.settings.timePerShape;
-
-    
 
     $(function() {
 
@@ -44,22 +43,19 @@ GAME.timer = function(jQuery, shapeNumber){
 
     }
 
- 
-
     // Common functions
-function pad(number, length) {
-    var str = '' + number;
-    while (str.length < length) {str = '0' + str;}
-    return str;
-}
-function formatTime(time) {
-    time = time / 10;
-    var min = parseInt(time / 6000),
-        sec = parseInt(time / 100) - (min * 60),
-        hundredths = pad(time - (sec * 100) - (min * 6000), 2);
-    return (min > 0 ? pad(min, 2) : "00") + ":" + pad(sec, 2) + ":" + hundredths;
-}
+    function pad(number, length) {
+        var str = '' + number;
+        while (str.length < length) {str = '0' + str;}
+        return str;
+    }
 
-
+    function formatTime(time) {
+        time = time / 10;
+        var min = parseInt(time / 6000),
+            sec = parseInt(time / 100) - (min * 60),
+            hundredths = pad(time - (sec * 100) - (min * 6000), 2);
+        return (min > 0 ? pad(min, 2) : "00") + ":" + pad(sec, 2) + ":" + hundredths;
+    }
 
 }(jQuery, window.GAME.settings.numShapes, undefined);
